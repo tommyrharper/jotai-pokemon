05267ddfa7da96942371c7c12a6339718f67e7bb
tommyrharper <thomasrobertharper@gmail.com>
2021-05-11 03:39:08 +0100

    Working magiql

--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import {
+  GraphQLClientProvider,
+  GraphQLClient,
+  useQuery,
+  graphql,
+} from "magiql";
+
+const client = new GraphQLClient({
+  endpoint: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+});
+
+const GET_PEOPLE = graphql`
+  query PeopleQuery($limit: Int) {
+    allPeople(first: $limit) {
+      edges {
+        node {
+          id
+          name
+          homeworld {
+            name
+          }
+        }
+      }
+    }
+  }
+`
+
+const People = () => {
+  const { data, status, error } = useQuery(
+    GET_PEOPLE,
+    {
+      variables: {
+        limit: 10,
+      },
+    }
+  );
+
+  console.log(`data`, data)
+  console.log(`status`, status)
+  console.log(`error`, error)
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <div>
+      {data
+        ? data.allPeople?.edges?.map((edge) => (
+            <div key={edge.node.id}>
+              <b>{edge.node.name}</b> ({edge.node.homeworld?.name})
+            </div>
+          ))
+        : null}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <GraphQLClientProvider client={client}>
+      <People />
+    </GraphQLClientProvider>
+  );
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GraphQLClientProvider client={client}>
+      <App />
+    </GraphQLClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
